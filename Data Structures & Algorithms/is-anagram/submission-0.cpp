01d66ae495fceb129897441153bd11985741fa92
Chanchal3272 <chanchalpatle14@gmail.com>
2026-09-06 12:15:24 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>hash1(26,0);
+        vector<int>hash2(26,0);
+        for(auto&it:s){
+            hash1[it-'a']++;
+        }
+        for(auto&it:t){
+            hash2[it-'a']++;
+        }
+        for(int i = 0;i<=25;i++){
+            if(hash1[i]!=hash2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int>mpp;
+        for(int i = 0;i<n;i++){
+            int a = nums[i];
+            int rem = target - a;
+            if(mpp.find(rem)!=mpp.end()){
+                return {mpp[rem],i};
+            }
+            mpp[a]=i;
+        }
+    }
+};
